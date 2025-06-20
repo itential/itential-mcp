e9d85f570718766d461867d16b63ac86784b285c
Peter Sprygada <privateip@users.noreply.github.com>
2025-06-20 16:23:07 -0400

    Adds configuration file and tags to the application (#48)
    This feature adds two signifcant changes to the MCP server.  First it
    adds a new configuration file option when starting the sever.  To use a
    configuration file, add the `--config` command line option and point it
    to the desired configuration file.  There is an annotated configuration
    file example avaliable at `docs/mcp.conf.example`
    
    This also adds support for include and exclude tags to the application.
    Include and exclude tags can be used to explictly include or exclude
    tools when the server starts.  This allows for different tools to be
    available and map to different authentications to the server

--- src/itential_mcp/config.py
+++ src/itential_mcp/config.py
@@ -1,37 +1,115 @@
 # Copyright (c) 2025 Itential, Inc
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+import os
+import configparser
+
+from functools import lru_cache
+from pathlib import Path
+
+from typing import Literal
+from dataclasses import fields
+
+from pydantic import Field
+from pydantic.dataclasses import dataclass
+
 from . import env
 
 
-def get() -> dict:
-    """
-    Load the configuration from the environment
+@dataclass(frozen=True)
+class Config(object):
 
-    This function will load the Platform configuration from the environment
-    and return it as a Python dict object.  The returned object can be passed
-    into the `platform_factory(...)` function to create a new instance.
+    server_transport: Literal["stdio", "sse"] = Field(default="stdio")
+    server_host: str = Field(default="localhost")
+    server_port: int = Field(default=0)
+    server_log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = Field(default="INFO")
+    server_include_tags: str | None = Field(default=None)
+    server_exclude_tags: str | None = Field(default="experimental,beta")
+
+    platform_host: str = Field(default="localhost")
+    platform_port: int = Field(default=0)
+    platform_disable_tls: bool = Field(default=False)
+    platform_disable_verify: bool = Field(default=False)
+    platform_user: str = Field(default="admin")
+    platform_password: str = Field(default="admin")
+    platform_client_id: str | None = Field(default=None)
+    platform_client_secret: str | None = Field(default=None)
+    platform_timeout: int = Field(default=30)
+
+    @property
+    def server(self):
+        return {
+            "transport": self.server_transport,
+            "host": self.server_host,
+            "port": self.server_port,
+            "log_level": self.server_log_level,
+            "include_tags": self._coerce_to_set(self.server_include_tags) if self.server_include_tags else None,
+            "exclude_tags": self._coerce_to_set(self.server_exclude_tags) if self.server_exclude_tags else None
+        }
+
+    @property
+    def platform(self):
+        return {
+            "host": self.platform_host,
+            "port": self.platform_port,
+            "use_tls": not self.platform_disable_tls,
+            "verify": not self.platform_disable_verify,
+            "user": self.platform_user,
+            "password": self.platform_password,
+            "client_id": None if self.platform_client_id == "" else self.platform_client_id,
+            "client_secret": None if self.platform_client_secret == "" else self.platform_client_secret,
+            "timeout": self.platform_timeout
+        }
+
+    def _coerce_to_set(self, value) -> list:
+        items = set()
+        for ele in value.split(","):
+            items.add(ele.strip())
+        return items
+
+
+@lru_cache(maxsize=None)
+def get() -> Config:
+    """
+    Return the configuration instance
+
+    This function will load the configuration and return an instance of
+    Config.  This function is cached and is safe to call multiple times.
+    The configuration is loaded only once and the cached Config instance
+    is returned with every call.
 
     Args:
         None
 
     Returns:
-        dict: Returns a Python dict object that represents the configuration
+        Conig: An instance of Config that represents the application
+            configuration
 
     Raises:
-        None
+        FileNotFoundError: If a configuration file is specified but not found
+            this exception is raised
     """
-    return {
-        "host": env.getstr("ITENTIAL_MCP_PLATFORM_HOST", default="localhost"),
-        "port": env.getint("ITENTIAL_MCP_PLATFORM_PORT", default=0),
+    conf_file = env.getstr("ITENTIAL_MCP_CONFIG")
 
-        "use_tls": not env.getbool("ITENTIAL_MCP_PLATFORM_DISABLE_TLS", default=False),
-        "verify": not env.getbool("ITENTIAL_MCP_PLATFORM_DISABLE_VERIFY", default=False),
-        "timeout": env.getint("ITENTIAL_MCP_PLATFORM_TIMEOUT", default=30),
+    data = {}
 
-        "user": env.getstr("ITENTIAL_MCP_PLATFORM_USER", default="admin"),
-        "password": env.getstr("ITENTIAL_MCP_PLATFORM_PASSWORD", default="admin"),
+    if conf_file is not None:
+        path = Path(conf_file)
+        if not path.is_file():
+            raise FileNotFoundError(f"Config file not found: {path}")
 
-        "client_id": env.getstr("ITENTIAL_MCP_PLATFORM_CLIENT_ID"),
-        "client_secret": env.getstr("ITENTIAL_MCP_PLATFORM_CLIENT_SECRET"),
-    }
+        cf = configparser.ConfigParser()
+        cf.read(conf_file)
+
+        for item in cf.sections():
+            for key, value in cf.items(item):
+                key = f"{item}_{key}"
+                data[key] = value
+
+    for item in fields(Config):
+        envkey = f"ITENTIAL_MCP_{item.name}".upper()
+        if envkey in os.environ:
+            value = ", ".join(os.environ[envkey].split(","))
+            data[item.name] = value
+
+    return Config(**data)
